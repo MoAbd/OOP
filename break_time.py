bced586c2a55698126d74d4c07e7158b27a9c9df
Mohamed Abdallah <mo.ab.elkasaby@gmail.com>
2016-10-20 16:34:39 +0200

    Add break_time file

---
+++ break_time.py
@@ -0,0 +1,16 @@
+import time 
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while break_count < total_breaks :
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=wWFJw5TnQgs")
+	break_count += 1
+
+
+
+
+
