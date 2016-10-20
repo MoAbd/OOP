44cf04218c4b71e5fb0208094036dc3eedfba278
Mohamed Abdallah <mo.ab.elkasaby@gmail.com>
2016-10-21 00:18:07 +0300

    Add comments

--- break_time.py
+++ break_time.py
@@ -1,13 +1,13 @@
 import time 
 import webbrowser
 
-total_breaks = 3
-break_count = 0
+total_breaks = 3 					#initialize number of breaks 
+break_count = 0 					#counter initialization
 
-print("This program started on " + time.ctime())
+print("This program started on " + time.ctime()) 	#printing the starting time 
 while break_count < total_breaks :
-	time.sleep(1*360*360)
-	webbrowser.open("https://www.youtube.com/watch?v=wWFJw5TnQgs")
+	time.sleep(1*360*360) 				#wait an hour 
+	webbrowser.open("https://www.youtube.com/watch?v=wWFJw5TnQgs") #open browser and play ur favourite vibe 
 	break_count += 1
 
 
