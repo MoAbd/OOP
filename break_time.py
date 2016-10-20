a8c8fafc8b5fee23ff22f1d9997565547a4a7253
Mohamed Abdallah <mo.ab.elkasaby@gmail.com>
2016-10-21 00:12:52 +0300

    Change waiting time to an hour

--- break_time.py
+++ break_time.py
@@ -6,7 +6,7 @@ break_count = 0
 
 print("This program started on " + time.ctime())
 while break_count < total_breaks :
-	time.sleep(10)
+	time.sleep(1*360*360)
 	webbrowser.open("https://www.youtube.com/watch?v=wWFJw5TnQgs")
 	break_count += 1
 
